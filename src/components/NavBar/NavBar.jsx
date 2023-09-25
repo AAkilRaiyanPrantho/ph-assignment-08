24531f4a8a09b52d9f475fddd0486f8bcf60c81f
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-09-26 00:04:24 +0600

    Banner made responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <div>
-        <div className="navbar bg-transparent flex flex-row justify-between px-[138px] py-[50px] relative z-[1]">
-        
-            <figure>
-            <img src="/Resources/Logo.png" alt="Logo"/>
-            </figure>
-            <div><ul className="flex text-[#0B0B0B] font-bold justify-between items-center gap-4"><li><Link to='/'>Home</Link></li>
-            <li><Link to='/donations'>Donation</Link></li>
-            <li><Link to='/statistics'>Statistics</Link></li></ul></div>
-            
+      <div className="navbar flex flex-col lg:flex-row justify-between px-[138px] py-[50px] relative z-[1]">
+        <figure>
+          <img src="/Resources/Logo.png" alt="Logo" />
+        </figure>
+        <div>
+          <ul className="flex flex-col my-10 text-[#0B0B0B] font-bold justify-between items-center gap-4 lg:flex-row">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/donations">Donation</NavLink>
+            </li>
+            <li>
+              <NavLink to="/statistics">Statistics</NavLink>
+            </li>
+          </ul>
         </div>
-      
+      </div>
     </div>
-      
-    
   );
 };
 

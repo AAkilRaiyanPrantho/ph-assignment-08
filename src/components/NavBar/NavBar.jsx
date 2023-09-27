b5c6415662b68c8a7a240e660cd27a5e0138ac63
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-09-27 23:00:41 +0600

    Dist File added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div>
       <div className="navbar flex flex-col lg:flex-row justify-between px-[138px] py-[50px] relative z-[1]">
         <figure>
-          <img src="/Resources/Logo.png" alt="Logo" />
+          <img src="https://i.ibb.co/FkQ9qrT/logo.png" alt="Logo" />
         </figure>
         <div>
           <ul className="flex flex-col my-10 text-[#0B0B0B] font-bold justify-between items-center gap-4 lg:flex-row">
